6d89265338691b1d1d424cf87e72c6b7b61e27b7
ganeshdeveloper3 <ganeshdeveloper3@gmail.com>
2021-08-01 16:15:24 +0530

    ActivtyNavigation test

--- app/src/androidTest/java/com/androiduitesting/MainActivityTest.kt
+++ app/src/androidTest/java/com/androiduitesting/MainActivityTest.kt
@@ -2,6 +2,8 @@ package com.androiduitesting
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.pressBack
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -44,5 +46,17 @@ class MainActivityTest{
             onView(withId(R.id.activity_main_title))
                 .check(matches(withText(R.string.text_mainactivity)))
         }
+
+    @Test
+    fun test_navSecondaryActvity() {
+
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.button_next_activity)).perform(click())
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_back)).perform(click())
+
+    }
+
 
 }

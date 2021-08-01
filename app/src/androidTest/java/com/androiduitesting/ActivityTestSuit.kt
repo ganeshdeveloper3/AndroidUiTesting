6dd3eb554912ac60a3ed00375e35d5e5596a1d1c
ganeshdeveloper3 <ganeshdeveloper3@gmail.com>
2021-08-01 16:23:41 +0530

    Test suit examples

---
+++ app/src/androidTest/java/com/androiduitesting/ActivityTestSuit.kt
@@ -0,0 +1,12 @@
+package com.androiduitesting
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    MainActivityTest::class,
+    SecondaryActivityTest::class
+)
+class ActivityTestSuit {
+}
